51f9768a7cbd922f170160811d65a1e73544392b
William Lord <lordwilliam94@gmail.com>
2019-02-24 00:35:41 +1100

    minor edits

--- Queue/queue.c
+++ Queue/queue.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <assert.h>
 #include "queue.h"
+#include "..\Utilities\utils.h"
 
 
 /*****************************************************************************/
@@ -13,19 +14,20 @@
  *
  * Returns: pointer to queue
  */
-queue_t* create_empty_queue(int size){
+queue_t* create_empty_queue(int size)
+{
     if (size < 0){
         fprintf(stderr, "Queue size must be positive, not %d\n", size);
         assert(0 && "Queue size negative");
     }
     queue_t* q = malloc(sizeof(*q));
-    assert(q != NULL);
+    assert(unwanted_null(q));
     q->len = 0;
     q->front = 0;
     q->back = 0;
     q->alloc = size;
     q->queue = malloc(sizeof(*q->queue)*q->alloc);
-    assert(q->queue != NULL);
+    assert(unwanted_null(q->queue));
 
     q->is_empty = &is_empty_queue;
     q->enqueue = &enqueue;
@@ -43,7 +45,8 @@ queue_t* create_empty_queue(int size){
  *
  * Returns: 1 if empty, 0 otherwise.
  */
-int is_empty_queue(queue_t* queue){
+int is_empty_queue(queue_t* queue)
+{
     assert(queue != NULL);
     return (queue->len == 0);
 }
@@ -58,12 +61,13 @@ int is_empty_queue(queue_t* queue){
  *
  * Returns: void
  */
-void enqueue(queue_t *queue, int data){
+void enqueue(queue_t *queue, int data)
+{
     assert(queue != NULL);
     if (queue->len == queue->alloc){
         queue->alloc *= 2;
         queue->queue = realloc(queue->queue, queue->alloc*sizeof(int));
-        assert(queue->queue != NULL);
+        assert(unwanted_null(queue->queue));
         if (queue->back < (queue->alloc)/2){
             int i;
             for(i=0; i<queue->back; i++){
@@ -89,7 +93,8 @@ void enqueue(queue_t *queue, int data){
  *
  * Returns: first element of queue: an integer
  */
-int dequeue(queue_t *queue){
+int dequeue(queue_t *queue)
+{
     assert(queue != NULL);
     int to_return = queue->queue[queue->front++];
     queue->len--;
@@ -107,7 +112,8 @@ int dequeue(queue_t *queue){
  *
  * Returns: void
  */
-void destroy_queue(queue_t* q){
+void destroy_queue(queue_t* q)
+{
     if (q == NULL){
         return;
     }
@@ -122,7 +128,8 @@ void destroy_queue(queue_t* q){
 //-----------------------------------------------------------------------------
 
 
-void print_queue(queue_t* queue){
+void print_queue(queue_t* queue)
+{
     assert(queue != NULL);
     int i;
     if (queue->back > queue->front){
@@ -141,7 +148,8 @@ void print_queue(queue_t* queue){
 
 }
 
-void print_all(queue_t* queue){
+void print_all(queue_t* queue)
+{
     int i;
     for(i=0; i<queue->alloc; i++){
         printf("%d ", queue->queue[i]);
